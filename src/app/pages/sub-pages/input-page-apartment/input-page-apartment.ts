4f95adadac81494b828f469d0016afa8a199ead6
ericB1708 <eric.berndt2001@googlemail.com>
2026-05-11 23:06:45 +0200

    create input page apartment and result page apartment

---
+++ src/app/pages/sub-pages/input-page-apartment/input-page-apartment.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-page-apartment',
+  imports: [],
+  templateUrl: './input-page-apartment.html',
+  styleUrl: './input-page-apartment.scss',
+})
+export class InputPageApartment {
+
+}
---
+++ src/app/pages/sub-pages/result-page-apartment/result-page-apartment.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-page-apartment',
+  imports: [],
+  templateUrl: './result-page-apartment.html',
+  styleUrl: './result-page-apartment.scss',
+})
+export class ResultPageApartment {
+
+}
